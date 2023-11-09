c9dcfe63b2c4b0375a7799cd484416625d93ac19
raoufchebri <raouf@chebri.com>
2023-11-09 07:29:24 +0200

    add-symbol

---
+++ prisma/migrations/20231109052736_add_symbol/migration.sql
@@ -0,0 +1,476 @@
+-- AlterTable
+ALTER TABLE "atomic_element" ADD COLUMN     "symbol" TEXT;
+
+-- ALTER TABLE atomic_element ADD COLUMN symbol TEXT;
+
+UPDATE atomic_element
+SET symbol = 'H'
+WHERE atomic_number = 1;
+
+UPDATE atomic_element
+SET symbol = 'He'
+WHERE atomic_number = 2;
+
+UPDATE atomic_element
+SET symbol = 'Li'
+WHERE atomic_number = 3;
+
+UPDATE atomic_element
+SET symbol = 'Be'
+WHERE atomic_number = 4;
+
+UPDATE atomic_element
+SET symbol = 'B'
+WHERE atomic_number = 5;
+
+UPDATE atomic_element
+SET symbol = 'C'
+WHERE atomic_number = 6;
+
+UPDATE atomic_element
+SET symbol = 'N'
+WHERE atomic_number = 7;
+
+UPDATE atomic_element
+SET symbol = 'O'
+WHERE atomic_number = 8;
+
+UPDATE atomic_element
+SET symbol = 'F'
+WHERE atomic_number = 9;
+
+UPDATE atomic_element
+SET symbol = 'Ne'
+WHERE atomic_number = 10;
+
+UPDATE atomic_element
+SET symbol = 'Na'
+WHERE atomic_number = 11;
+
+UPDATE atomic_element
+SET symbol = 'Mg'
+WHERE atomic_number = 12;
+
+UPDATE atomic_element
+SET symbol = 'Al'
+WHERE atomic_number = 13;
+
+UPDATE atomic_element
+SET symbol = 'Si'
+WHERE atomic_number = 14;
+
+UPDATE atomic_element
+SET symbol = 'P'
+WHERE atomic_number = 15;
+
+UPDATE atomic_element
+SET symbol = 'S'
+WHERE atomic_number = 16;
+
+UPDATE atomic_element
+SET symbol = 'Cl'
+WHERE atomic_number = 17;
+
+UPDATE atomic_element
+SET symbol = 'Ar'
+WHERE atomic_number = 18;
+
+UPDATE atomic_element
+SET symbol = 'K'
+WHERE atomic_number = 19;
+
+UPDATE atomic_element
+SET symbol = 'Ca'
+WHERE atomic_number = 20;
+
+UPDATE atomic_element
+SET symbol = 'Sc'
+WHERE atomic_number = 21;
+
+UPDATE atomic_element
+SET symbol = 'Ti'
+WHERE atomic_number = 22;
+
+UPDATE atomic_element
+SET symbol = 'V'
+WHERE atomic_number = 23;
+
+UPDATE atomic_element
+SET symbol = 'Cr'
+WHERE atomic_number = 24;
+
+UPDATE atomic_element
+SET symbol = 'Mn'
+WHERE atomic_number = 25;
+
+UPDATE atomic_element
+SET symbol = 'Fe'
+WHERE atomic_number = 26;
+
+UPDATE atomic_element
+SET symbol = 'Co'
+WHERE atomic_number = 27;
+
+UPDATE atomic_element
+SET symbol = 'Ni'
+WHERE atomic_number = 28;
+
+UPDATE atomic_element
+SET symbol = 'Cu'
+WHERE atomic_number = 29;
+
+UPDATE atomic_element
+SET symbol = 'Zn'
+WHERE atomic_number = 30;
+
+UPDATE atomic_element
+SET symbol = 'Ga'
+WHERE atomic_number = 31;
+
+UPDATE atomic_element
+SET symbol = 'Ge'
+WHERE atomic_number = 32;
+
+UPDATE atomic_element
+SET symbol = 'As'
+WHERE atomic_number = 33;
+
+UPDATE atomic_element
+SET symbol = 'Se'
+WHERE atomic_number = 34;
+
+UPDATE atomic_element
+SET symbol = 'Br'
+WHERE atomic_number = 35;
+
+UPDATE atomic_element
+SET symbol = 'Kr'
+WHERE atomic_number = 36;
+
+UPDATE atomic_element
+SET symbol = 'Rb'
+WHERE atomic_number = 37;
+
+UPDATE atomic_element
+SET symbol = 'Sr'
+WHERE atomic_number = 38;
+
+UPDATE atomic_element
+SET symbol = 'Y'
+WHERE atomic_number = 39;
+
+UPDATE atomic_element
+SET symbol = 'Zr'
+WHERE atomic_number = 40;
+
+UPDATE atomic_element
+SET symbol = 'Nb'
+WHERE atomic_number = 41;
+
+UPDATE atomic_element
+SET symbol = 'Mo'
+WHERE atomic_number = 42;
+
+UPDATE atomic_element
+SET symbol = 'Tc'
+WHERE atomic_number = 43;
+
+UPDATE atomic_element
+SET symbol = 'Ru'
+WHERE atomic_number = 44;
+
+UPDATE atomic_element
+SET symbol = 'Rh'
+WHERE atomic_number = 45;
+
+UPDATE atomic_element
+SET symbol = 'Pd'
+WHERE atomic_number = 46;
+
+UPDATE atomic_element
+SET symbol = 'Ag'
+WHERE atomic_number = 47;
+
+UPDATE atomic_element
+SET symbol = 'Cd'
+WHERE atomic_number = 48;
+
+UPDATE atomic_element
+SET symbol = 'In'
+WHERE atomic_number = 49;
+
+UPDATE atomic_element
+SET symbol = 'Sn'
+WHERE atomic_number = 50;
+
+UPDATE atomic_element
+SET symbol = 'Sb'
+WHERE atomic_number = 51;
+
+UPDATE atomic_element
+SET symbol = 'Te'
+WHERE atomic_number = 52;
+
+UPDATE atomic_element
+SET symbol = 'I'
+WHERE atomic_number = 53;
+
+UPDATE atomic_element
+SET symbol = 'Xe'
+WHERE atomic_number = 54;
+
+UPDATE atomic_element
+SET symbol = 'Cs'
+WHERE atomic_number = 55;
+
+UPDATE atomic_element
+SET symbol = 'Ba'
+WHERE atomic_number = 56;
+
+UPDATE atomic_element
+SET symbol = 'La'
+WHERE atomic_number = 57;
+
+UPDATE atomic_element
+SET symbol = 'Ce'
+WHERE atomic_number = 58;
+
+UPDATE atomic_element
+SET symbol = 'Pr'
+WHERE atomic_number = 59;
+
+UPDATE atomic_element
+SET symbol = 'Nd'
+WHERE atomic_number = 60;
+
+UPDATE atomic_element
+SET symbol = 'Pm'
+WHERE atomic_number = 61;
+
+UPDATE atomic_element
+SET symbol = 'Sm'
+WHERE atomic_number = 62;
+
+UPDATE atomic_element
+SET symbol = 'Eu'
+WHERE atomic_number = 63;
+
+UPDATE atomic_element
+SET symbol = 'Gd'
+WHERE atomic_number = 64;
+
+UPDATE atomic_element
+SET symbol = 'Tb'
+WHERE atomic_number = 65;
+
+UPDATE atomic_element
+SET symbol = 'Dy'
+WHERE atomic_number = 66;
+
+UPDATE atomic_element
+SET symbol = 'Ho'
+WHERE atomic_number = 67;
+
+UPDATE atomic_element
+SET symbol = 'Er'
+WHERE atomic_number = 68;
+
+UPDATE atomic_element
+SET symbol = 'Tm'
+WHERE atomic_number = 69;
+
+UPDATE atomic_element
+SET symbol = 'Yb'
+WHERE atomic_number = 70;
+
+UPDATE atomic_element
+SET symbol = 'Lu'
+WHERE atomic_number = 71;
+
+UPDATE atomic_element
+SET symbol = 'Hf'
+WHERE atomic_number = 72;
+
+UPDATE atomic_element
+SET symbol = 'Ta'
+WHERE atomic_number = 73;
+
+UPDATE atomic_element
+SET symbol = 'W'
+WHERE atomic_number = 74;
+
+UPDATE atomic_element
+SET symbol = 'Re'
+WHERE atomic_number = 75;
+
+UPDATE atomic_element
+SET symbol = 'Os'
+WHERE atomic_number = 76;
+
+UPDATE atomic_element
+SET symbol = 'Ir'
+WHERE atomic_number = 77;
+
+UPDATE atomic_element
+SET symbol = 'Pt'
+WHERE atomic_number = 78;
+
+UPDATE atomic_element
+SET symbol = 'Au'
+WHERE atomic_number = 79;
+
+UPDATE atomic_element
+SET symbol = 'Hg'
+WHERE atomic_number = 80;
+
+UPDATE atomic_element
+SET symbol = 'Tl'
+WHERE atomic_number = 81;
+
+UPDATE atomic_element
+SET symbol = 'Pb'
+WHERE atomic_number = 82;
+
+UPDATE atomic_element
+SET symbol = 'Bi'
+WHERE atomic_number = 83;
+
+UPDATE atomic_element
+SET symbol = 'Po'
+WHERE atomic_number = 84;
+
+UPDATE atomic_element
+SET symbol = 'At'
+WHERE atomic_number = 85;
+
+UPDATE atomic_element
+SET symbol = 'Rn'
+WHERE atomic_number = 86;
+
+UPDATE atomic_element
+SET symbol = 'Fr'
+WHERE atomic_number = 87;
+
+UPDATE atomic_element
+SET symbol = 'Ra'
+WHERE atomic_number = 88;
+
+UPDATE atomic_element
+SET symbol = 'Ac'
+WHERE atomic_number = 89;
+
+UPDATE atomic_element
+SET symbol = 'Th'
+WHERE atomic_number = 90;
+
+UPDATE atomic_element
+SET symbol = 'Pa'
+WHERE atomic_number = 91;
+
+UPDATE atomic_element
+SET symbol = 'U'
+WHERE atomic_number = 92;
+
+UPDATE atomic_element
+SET symbol = 'Np'
+WHERE atomic_number = 93;
+
+UPDATE atomic_element
+SET symbol = 'Pu'
+WHERE atomic_number = 94;
+
+UPDATE atomic_element
+SET symbol = 'Am'
+WHERE atomic_number = 95;
+
+UPDATE atomic_element
+SET symbol = 'Cm'
+WHERE atomic_number = 96;
+
+UPDATE atomic_element
+SET symbol = 'Bk'
+WHERE atomic_number = 97;
+
+UPDATE atomic_element
+SET symbol = 'Cf'
+WHERE atomic_number = 98;
+
+UPDATE atomic_element
+SET symbol = 'Es'
+WHERE atomic_number = 99;
+
+UPDATE atomic_element
+SET symbol = 'Fm'
+WHERE atomic_number = 100;
+
+UPDATE atomic_element
+SET symbol = 'Md'
+WHERE atomic_number = 101;
+
+UPDATE atomic_element
+SET symbol = 'No'
+WHERE atomic_number = 102;
+
+UPDATE atomic_element
+SET symbol = 'Lr'
+WHERE atomic_number = 103;
+
+UPDATE atomic_element
+SET symbol = 'Rf'
+WHERE atomic_number = 104;
+
+UPDATE atomic_element
+SET symbol = 'Db'
+WHERE atomic_number = 105;
+
+UPDATE atomic_element
+SET symbol = 'Sg'
+WHERE atomic_number = 106;
+
+UPDATE atomic_element
+SET symbol = 'Bh'
+WHERE atomic_number = 107;
+
+UPDATE atomic_element
+SET symbol = 'Hs'
+WHERE atomic_number = 108;
+
+UPDATE atomic_element
+SET symbol = 'Mt'
+WHERE atomic_number = 109;
+
+UPDATE atomic_element
+SET symbol = 'Ds '
+WHERE atomic_number = 110;
+
+UPDATE atomic_element
+SET symbol = 'Rg '
+WHERE atomic_number = 111;
+
+UPDATE atomic_element
+SET symbol = 'Cn '
+WHERE atomic_number = 112;
+
+UPDATE atomic_element
+SET symbol = 'Nh'
+WHERE atomic_number = 113;
+
+UPDATE atomic_element
+SET symbol = 'Fl'
+WHERE atomic_number = 114;
+
+UPDATE atomic_element
+SET symbol = 'Mc'
+WHERE atomic_number = 115;
+
+UPDATE atomic_element
+SET symbol = 'Lv'
+WHERE atomic_number = 116;
+
+UPDATE atomic_element
+SET symbol = 'Ts'
+WHERE atomic_number = 117;
+
+UPDATE atomic_element
+SET symbol = 'Og'
+WHERE atomic_number = 118;
